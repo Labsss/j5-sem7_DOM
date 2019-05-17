23428825cdf933f0fdffc0301122f131cc77dbee
Labsss <alexandre.labonne@gmail.com>
2019-05-17 16:52:13 +0200

    exos DOM

---
+++ javascript-web/chapitre_2/js/cours.js
@@ -0,0 +1,31 @@
+var titresElts = document.getElementsByTagName("h2"); // Tous les titres h2
+console.log(titresElts[0]); // Affiche le premier titre h2
+console.log(titresElts.length); // Affiche 3
+
+// Tous les éléments ayant la classe "merveilles"
+var merveillesElts = document.getElementsByClassName("merveilles");
+for (var i = 0; i < merveillesElts.length; i++) {
+    console.log(merveillesElts[i]);
+}
+
+// Tous les paragraphes
+console.log(document.querySelectorAll("p").length); // Affiche 3
+
+// Tous les paragraphes à l'intérieur de l'élément identifié par "contenu"
+console.log(document.querySelectorAll("#contenu p").length); // Affiche 2
+
+// Tous les éléments ayant la classe "existe"
+console.log(document.querySelectorAll(".existe").length); // Affiche 8
+
+// Tous les éléments fils de l'élément identifié par "antiques" ayant la classe "existe"
+console.log(document.querySelectorAll("#antiques > .existe").length); // Affiche 1
+
+
+
+// Le premier paragraphe
+console.log(document.querySelector("p"));
+
+// Le premier paragraphe
+console.log(document.querySelector("p"));
+
+
